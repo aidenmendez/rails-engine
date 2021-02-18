2c3c7f7a862e529bb05d1c5d455d56fa53e5aa91
Aiden Mendez <aidenmendez@gmail.com>
2021-02-17 22:05:21 -0700

    Set up tests for get all items endpoint and fix item factory

---
+++ spec/requests/api/v1/items/items_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe "Items", type: :request do
+  describe "get All Items" do
+    describe "(happy path)" do
+      it "returns all existing items" do
+        create(:merchant)
+        item1 = create(:item)
+        item2 = create(:item)
+        item3 = create(:item)
+
+        get api_v1_items_path
+        expect(response.status).to eq(200)
+
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        expect(json[:data].count).to eq(3)
+        require 'pry'; binding.pry
+        expect(json[:data].first[])
+      end
+    end
+
+    describe "(sad path)" do
+      it "returns empty array if there are no items" do
+
+      end
+    end
+    
+  end
+
+  describe "get one item" do
+    describe "(happy path)" do
+      it "returns a single item when given an existing item id" do
+
+      end
+    end
+
+    describe "(sad path)" do
+      it "returns a 404 error when given a bad item id" do
+
+      end
+    end
+  end
+
+  describe "Get One Item" do
+
+  end
+end
