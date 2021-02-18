9cf016c9af49faad2131637b6d8db7d9c1012b8c
Aiden Mendez <aidenmendez@gmail.com>
2021-02-18 15:29:19 -0700

    Update test suite for item merchant spec

--- spec/requests/api/v1/items/merchants_spec.rb
+++ spec/requests/api/v1/items/merchants_spec.rb
@@ -7,28 +7,28 @@ RSpec.describe "Merchants", type: :request do
         merchant = create(:merchant)
         item = create(:item)
 
-        get api_v1_item_merchant_path(item)
-        expect(response).to eq(200)
+        get api_v1_item_merchant_index_path(item)
+        expect(response.status).to eq(200)
 
         json = JSON.parse(response.body)
-        expect(json[:data][:merchant_id]).to eq(merchant.id)
+        expect(json["data"]["id"].to_i).to eq(merchant.id)
       end
     end
 
     describe "(sad path)" do
       it "returns 404 error given incorrect item id" do
-        get api_v1_item_merchant_path(0)
-        expect(response).to eq(404)
+        get api_v1_item_merchant_index_path(0)
+        expect(response.status).to eq(404)
 
-        get api_v1_item_merchant_path(1000)
-        expect(response).to eq(404)
+        get api_v1_item_merchant_index_path(1000)
+        expect(response.status).to eq(404)
       end
       it "returns 404 error when item id is string" do
         merchant = create(:merchant)
         item = create(:item)
 
-        get api_v1_item_merchant_path(item.id.to_s)
-        expect(response).to eq(404)
+        get api_v1_item_merchant_index_path(item.id.to_s)
+        expect(response.status).to eq(404)
       end
     end
   end
