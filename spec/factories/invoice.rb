6668c8484d9eac01803377d2436f507d7569317c
Aiden Mendez <aidenmendez@gmail.com>
2021-03-16 23:18:41 -0600

    Set up test data and create factories for each table

--- spec/factories/invoice.rb
+++ spec/factories/invoice.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :item do
+  factory :invoice do
     customer_id { Customer.order('RANDOM()').first.id }
     merchant_id { Merchant.order('RANDOM()').first.id }
     status      { rand(0...3) }
--- spec/requests/api/v1/merchants/find_merchants_who_sold_most_items_spec.rb
+++ spec/requests/api/v1/merchants/find_merchants_who_sold_most_items_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Merchants Most Items Request", type: :request do
   before(:each) do
+    10.times do
+      create(:customer)
+    end
+
     merchant1 = create(:merchant, name: "The Tool Shed")
     merchant2 = create(:merchant, name: "Cool Cats Hats")
     merchant3 = create(:merchant, name: "Socks Incorporated")
@@ -9,16 +13,45 @@ RSpec.describe "Merchants Most Items Request", type: :request do
     merchant5 = create(:merchant, name: "Water Bottle Co")
     merchant6 = create(:merchant, name: "Pictures, Framed")
     merchant7 = create(:merchant, name: "Sweater Weather")
-    item1 = create(:merchant => merchant1.id)
-    item2 = create(:merchant => merchant1.id)
-    item3 = create(:merchant => merchant2.id)
-    item4 = create(:merchant => merchant3.id)
-    item5 = create(:merchant => merchant3.id)
-    item6 = create(:merchant => merchant4.id)
-    item7 = create(:merchant => merchant5.id)
-    item8 = create(:merchant => merchant6.id)
-    item8 = create(:merchant => merchant7.id)
-    
+
+    item1 = create(:item, :merchant_id => merchant1.id)
+    item2 = create(:item, :merchant_id => merchant1.id)
+    item3 = create(:item, :merchant_id => merchant2.id)
+    item4 = create(:item, :merchant_id => merchant3.id)
+    item5 = create(:item, :merchant_id => merchant3.id)
+    item6 = create(:item, :merchant_id => merchant4.id)
+    item7 = create(:item, :merchant_id => merchant5.id)
+    item8 = create(:item, :merchant_id => merchant6.id)
+    item9 = create(:item, :merchant_id => merchant7.id)
+
+    invoice1 = create(:invoice, :merchant_id => merchant1.id, :status => 2)
+    invoice2 = create(:invoice, :merchant_id => merchant2.id, :status => 2)
+    invoice3 = create(:invoice, :merchant_id => merchant2.id, :status => 2)
+    invoice4 = create(:invoice, :merchant_id => merchant3.id, :status => 2)
+    invoice5 = create(:invoice, :merchant_id => merchant4.id, :status => 2)
+    invoice6 = create(:invoice, :merchant_id => merchant4.id, :status => 2)
+    invoice7 = create(:invoice, :merchant_id => merchant5.id, :status => 2)
+    invoice8 = create(:invoice, :merchant_id => merchant6.id, :status => 2)
+    invoice9 = create(:invoice, :merchant_id => merchant6.id, :status => 2)
+    invoice10 = create(:invoice, :merchant_id => merchant6.id, :status => 2)
+    invoice11 = create(:invoice, :merchant_id => merchant7.id, :status => 2)
+    invoice12 = create(:invoice, :merchant_id => merchant7.id, :status => 2)
+
+    invoice_item1 = create(:invoice_item, :invoice_id => invoice8.id, :item_id => item8.id, :quantity => 5000)
+    invoice_item2 = create(:invoice_item, :invoice_id => invoice9.id, :item_id => item8.id, :quantity => 300)
+    invoice_item3 = create(:invoice_item, :invoice_id => invoice11.id, :item_id => item9.id, :quantity => 2500)
+    invoice_item4 = create(:invoice_item, :invoice_id => invoice2.id, :item_id => item3.id, :quantity => 5000)
+    invoice_item5 = create(:invoice_item, :invoice_id => invoice1.id, :item_id => item1.id, :quantity => 150)
+    invoice_item6 = create(:invoice_item, :invoice_id => invoice5.id, :item_id => item6.id, :quantity => 700)
+    invoice_item7 = create(:invoice_item, :invoice_id => invoice4.id, :item_id => item5.id, :quantity => 10)
+
+    purchase1 = create(:purchase, :invoice_id => invoice8.id, :result => 2)
+    purchase2 = create(:purchase, :invoice_id => invoice9.id, :result => 2)
+    purchase3 = create(:purchase, :invoice_id => invoice11.id, :result => 2)
+    purchase4 = create(:purchase, :invoice_id => invoice2.id, :result => 2)
+    purchase5 = create(:purchase, :invoice_id => invoice1.id, :result => 2)
+    purchase6 = create(:purchase, :invoice_id => invoice5.id, :result => 2)
+    purchase7 = create(:purchase, :invoice_id => invoice4.id, :result => 2)
   end
 
   it "fetches merchants who sold most items (no quantity provided)" do
