0f84f70eae1d5d1d5ccc1e30d345dee8233abc26
Aiden Mendez <aidenmendez@gmail.com>
2021-03-16 22:01:46 -0600

    Create invoice factory

---
+++ spec/factories/invoice.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :item do
+    customer_id { Customer.order('RANDOM()').first.id }
+    merchant_id { Merchant.order('RANDOM()').first.id }
+    status      { rand(0...3) }
+  end
+end
