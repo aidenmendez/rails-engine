6212d7e5bcdf52113377b0ec5353526bebfa0ec5
Aiden Mendez <aidenmendez@gmail.com>
2021-03-18 11:58:10 -0600

    Call merchant model method to find top merchants by items sold

--- app/controllers/api/v1/merchants/merchants_controller.rb
+++ app/controllers/api/v1/merchants/merchants_controller.rb
@@ -10,15 +10,23 @@ class Api::V1::Merchants::MerchantsController < ApplicationController
       merchant = Merchant.find(params[:id])
       render json: MerchantSerializer.new(merchant)
     rescue
-      render json: {"error" => {}}, status:404
+      render json: {"error" => {}}, status:404 
     end
   end
 
   def most_items
     begin
-      require 'pry'; binding.pry
-      merchants = Merchant.merchant_sold_most_items(params[:quantity])
-      render json: MerchantListSerializer.new(merchants)
+      if params[:quantity]
+        merchants = Merchant.merchant_sold_most_items(params[:quantity])
+      else
+        merchants = Merchant.merchant_sold_most_items
+      end
+
+      top_merchants = merchants.map do |merchant|
+        MerchantWithItemsSold.new(merchant)
+      end
+
+      render json: MerchantsWithItemsSoldSerializer.new(top_merchants), status:200
     rescue
 
     end
